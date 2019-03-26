ad0180141b100b7a0c671e15b0cc454eaea995d1
Trevor Rowe <tr968114@ohio.edu>
2019-03-26 10:51:04 -0400

    Added usability features
    -Can now take a command line argument with the path to the pfSense config file.  If no argument is provided, script will prompt for a path
    -Now has the ability to show the underlying interface names that correlate with the friendly names from the pfSense UI
    -Displays a menu with options to use added features

--- pfSenseRuleCloner.py
+++ pfSenseRuleCloner.py
@@ -1,15 +1,17 @@
 import xml.etree.ElementTree as ET
+from sys import argv, exit
 from copy import deepcopy
 
-clonedInt = 'wan'
-newInt = 'opt1'
-config = ET.parse("filter.xml")
+if len(argv) > 1: xmlPath = argv[1]
+else: xmlPath = input("Path to pfSense config file: ")
+
+
+config = ET.parse(xmlPath)
 configRoot = config.getroot()
 
 def isClonedInt(cloneInt, rule):
     ruleInt = rule.find('interface')
     if ruleInt == None:
-        print('false')
         return False
     return ruleInt.text == cloneInt
 
@@ -18,9 +20,41 @@ def cloneRule(clone2Int, rule):
     newRule.find('interface').text = clone2Int
     return newRule
 
-for rule in configRoot.findall('rule'):
-    if isClonedInt(clonedInt, rule):
-        newRule = cloneRule(newInt, rule)
-        configRoot.append(newRule)
+def printMenu():
+    print("Select an option here:")
+    print("1: Display pfSense interface names/friendly names")
+    print("2: Copy rules between interfaces")
+    print("3: Write changes to output file and exit")
+    print("4: Exit")
+    return input(">")
+
+def intClone():
+    clonedInt = input("Interface to clone from (pfSense name): ")
+    newInt = input("Interface to clone to (pfSense name): ")
+    filterElement = configRoot.find("filter")
+    for rule in filterElement.findall('rule'):
+        if isClonedInt(clonedInt, rule):
+            newRule = cloneRule(newInt, rule)
+            filterElement.append(newRule)
+            
+def friendlyNames():
+    interfaces = configRoot.find("interfaces")
+    print("Friendly name:   pfSense Name")
+    print("-----------------------------")
+    for interface in interfaces:
+        print(interface.find("descr").text + ":   " + interface.tag)
+            
+while True:
+    action = printMenu()
+    if action == "1":
+        friendlyNames()
+        input("Press enter to continue...")
+    elif action == "2":
+        intClone()
+    elif action == "3":
+        break
+    elif action == "4":
+        exit()
+                    
         
 config.write('out.xml')
